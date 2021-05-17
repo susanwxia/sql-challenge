08273ab01fc7f5a9fc8dbef2c355d93c43ee94b8
susanwxia <78228812+susanwxia@users.noreply.github.com>
2021-05-17 11:19:26 -0700

    Add files via upload

--- employee_tables.sql
+++ employee_tables.sql
@@ -14,19 +14,12 @@ CREATE TABLE departments(
 
 SELECT * FROM departments;
 
-CREATE TABLE dept_emp(	
-	emp_no INT, 
-	dept_no VARCHAR
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY,
+    title VARCHAR
 );
 
-SELECT * FROM dept_emp;
-
-CREATE TABLE dept_manager(	
-	dept_no VARCHAR,
-	emp_no INT
-);
-
-SELECT * FROM dept_manager;
+SELECT * FROM titles;
 
 CREATE TABLE employees(	
 	emp_no INT PRIMARY KEY,
@@ -35,23 +28,41 @@ CREATE TABLE employees(
 	first_name VARCHAR,
 	last_name VARCHAR,
 	sex VARCHAR,
-	hire_date DATE
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 SELECT * FROM employees;
 
-CREATE TABLE salaries(
+CREATE TABLE dept_emp(	
+	id SERIAL PRIMARY KEY,
+	emp_no INT, 
+	dept_no VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+CREATE TABLE dept_manager(	
+	id SERIAL PRIMARY KEY,
+	dept_no VARCHAR,
 	emp_no INT,
-    salary INT
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_manager;
+
+CREATE TABLE salaries(
+	id SERIAL PRIMARY KEY,
+	emp_no INT,
+    salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 SELECT * FROM salaries;
 
-CREATE TABLE titles(
-	title_id VARCHAR PRIMARY KEY,
-    title VARCHAR
-);
 
-SELECT * FROM titles;
 
 
